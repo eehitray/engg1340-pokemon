c85714a1917a5816070da468c1c742895ab43b84
Dhruv Aggarwal <aggdhruv.connect.hku.hk>
2020-04-01 03:46:24 +0530

    Added Move structure

---
+++ Move.h
@@ -0,0 +1,14 @@
+//Move.h
+
+#ifndef MOVE
+#define MOVE
+#include<string>
+
+struct Move 
+{
+	string name;
+	int damage;	//loss of health the move causes to opponent
+	double hit;	//probability of move being successful
+};
+
+#endif
